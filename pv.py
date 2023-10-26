b8bd817abb998deffae2e873482ae8b4609a0fa1
Maxwell <98100481+D-Maxwell@users.noreply.github.com>
2023-10-26 17:08:39 +0200

    {+} Hex <-> RGB colour format
    Press OK to switch

--- pv.py
+++ pv.py
@@ -3,63 +3,92 @@
 from math import ceil
 from kandinsky import *
 from hw import *
+from km import *
 
 
-for x in range(ceil(scrW/24)):
-  for y in range(ceil(scrH/24)):
-    fill_rect(x*24,y*24,24,24,[[100]*3,[200]*3][(x+y)%2])
+def alphaCheckerBoard(dim=[24]*2):
+  for x in range(ceil(scrW/dim[0])):
+    for y in range(ceil(scrH/dim[1])):
+      fill_rect(
+        x*dim[0],
+        y*dim[1],
+        dim[0],
+        dim[1],
+        [ [100]*3,[200]*3 ][(x+y)%2]
+      )
 
 
 
-box_width=3
-
-for c,color in enumerate(get_palette().items()):
+def pv():
   
-  fill_rect(
-    x:=ceil(scrW/box_width)*(c%box_width),
-    y:=(scrH//(len(get_palette())%box_width)) * (c//box_width),
-    w:=ceil(scrW/box_width),
-    h:=scrH//ceil(len(get_palette())/box_width),
-    color[1],
-  )
+  box_width=3
   
-  def contrast(clr1,clr2,bg=[0]*3):
-    diffs = [abs(sum([
-      [clr1,clr2][i][c] - bg[c]
-      for c in range(len(bg))
-    ]))
-    for i in range(2)]
+  for c,color in enumerate(get_palette().items()):
     
-    return [clr1,clr2][max(diffs)==diffs[1]]
+    fill_rect(
+      x:=ceil(scrW/box_width)*(c%box_width),
+      y:=(scrH//(len(get_palette())%box_width)) * (c//box_width),
+      w:=ceil(scrW/box_width),
+      h:=scrH//ceil(len(get_palette())/box_width),
+      color[1],
+    )
     
-  def caseSplit(text):
-    out=[]
-    idx=-1
-    for chr in text:
-      idx+=1
-      if idx==0 or not chr.isalpha(): continue
+    def contrast(clr1,clr2,bg=[0]*3):
+      diffs = [abs(sum([
+        [clr1,clr2][i][c] - bg[c]
+        for c in range(len(bg))
+      ]))
+      for i in range(2)]
       
-      if idx==len(text)-1:
+      return [clr1,clr2][max(diffs)==diffs[1]]
+      
+    def caseSplit(text):
+      out=[]
+      idx=-1
+      for chr in text:
         idx+=1
-            
-      if chr.isupper() or idx==len(text):
-        out+=[text[:idx]]
-        text=text[idx:]
-        idx=0
-      
-    return out
+        if idx==0 or not chr.isalpha(): continue
         
-  [draw_string(
-    bit,
-    x + w//2 - chrW*len(bit)//2,
-    y + h//2 - len(lines)*chrH//2 + b*chrH,
-    contrast(
-      get_palette()["PrimaryText"],
-      [0]*3,
-      bg=color[1]
-    ),
-    color[1],
-  ) for b,bit in enumerate(
-    lines:=caseSplit(str(color[0]))
-  )]
+        if idx==len(text)-1:
+          idx+=1
+              
+        if chr.isupper() or idx==len(text):
+          out+=[text[:idx]]
+          text=text[idx:]
+          idx=0
+        
+      return out
+        
+    [draw_string(
+      bit,
+      x + w//2 - chrW*len(bit)//2,
+      y + h//2 - len(lines)*chrH//2 + b*chrH,
+      contrast(
+        get_palette()["PrimaryText"],
+        [0]*3,
+        bg=color[1]
+      ),
+      color[1],
+    ) for b,bit in enumerate(
+      lines:=caseSplit(str(color[0]))
+      + (
+        ["#"+"".join([hex(channel)[2:] for channel in color[1]])]
+        if not COLOR_FORMAT else
+        [f"{'rgb'[c]}: {channel:3}" for c,channel in enumerate(color[1])]
+      )
+    )]
+
+
+COLOR_FORMAT=False
+
+alphaCheckerBoard()
+pv()
+
+while True:
+  
+  if keyin(KEY_OK):
+    COLOR_FORMAT^=True
+    pv()
+  
+
 
